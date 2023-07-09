ab79d08bf2e471262495f8a17407b4cf32edad35
Emerson Coronel <e.coronel@icloud.com>
2023-07-09 15:02:26 +0300

    Login and Dashboard pages

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'dashboard'  # Redirect to the dashboard page after successful login
+LOGIN_URL = 'login'  # Set the login URL to the login page
