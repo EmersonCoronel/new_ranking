336d77997e616f998b4202ca8ee26d3bbe1edcd0
Emerson Coronel <e.coronel@icloud.com>
2023-07-10 13:09:41 +0300

    Authentication works

--- mysite/new_ranking/forms.py
+++ mysite/new_ranking/forms.py
@@ -3,17 +3,15 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
-    username = forms.CharField(required=False)
-    email = forms.EmailField(required=True)
     full_name = forms.CharField(required=True)
 
     class Meta:
         model = User
-        fields = ("full_name", "email", "password1", "password2")
+        fields = ("full_name", "username", "password1", "password2")
 
     def save(self, commit=True):
         user = super(CustomUserCreationForm, self).save(commit=False)
-        user.email = self.cleaned_data['email']
+        user.username = self.cleaned_data['username']
         user.first_name = self.cleaned_data['full_name'].split()[0]
         user.last_name = ' '.join(self.cleaned_data['full_name'].split()[1:])
         if commit:
